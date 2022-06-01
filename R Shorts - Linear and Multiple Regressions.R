078bb0403ce1b10c537b6f80ca89aaa14abad9c3
Jeremy <100991718+JayScribes@users.noreply.github.com>
2022-06-01 23:28:48 +0800

    Add files via upload

---
+++ R Shorts - Linear and Multiple Regressions.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(psych)
+library(palmerpenguins)
+data(penguins)
+options(scipen = 999)
+
+
+pairs.panels(penguins[,c('bill_length_mm','bill_depth_mm','species','flipper_length_mm','island')])
+
+penguins.linear.regression <- lm(flipper_length_mm ~ bill_depth_mm, data=penguins)
+summary(penguins.linear.regression)
+
+
+
+penguins.multiple.regression <- lm(flipper_length_mm ~  bill_depth_mm + bill_length_mm, data = penguins)
+summary(penguins.multiple.regression)
+
+
+
+penguins.multiple.regression2 <- lm(flipper_length_mm ~  bill_depth_mm + bill_length_mm + species, data = penguins)
+summary(penguins.multiple.regression2)
+
